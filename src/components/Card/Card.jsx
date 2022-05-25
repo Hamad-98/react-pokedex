b5b3a8bae3dc7f0e099f159ca5295e94bc82f398
Hamad Ahmad <82972172+Hamad-98@users.noreply.github.com>
2022-05-24 22:56:01 -0400

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
-export default function Card() {
-  return <div>Card</div>;
+import "./Card.css";
+export default function Card(props) {
+  return (
+    <div className="cardContainer">
+      <img
+        className="cardIMG"
+        alt={props.current.name}
+        src={props.current.picture}
+      />
+      <span className="cardNAME">{props.current.name}</span>
+      <span className="cardDESCRIPTION">{props.current.description}</span>
+    </div>
+  );
 }
